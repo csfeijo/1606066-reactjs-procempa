c44ebfef1f8a8edeb9c8d9e3d6aab9690b43a30a
Cicero Feijo <cicero.feijo@gmail.com>
2022-10-05 09:09:08 -0300

    Fix das conf. do ESLint

--- curso-react-js/src/components/Loader/index.js
+++ curso-react-js/src/components/Loader/index.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 import svgLoader from '../../assets/img/loader.svg';
 import Container from './styles';
 
-const Loader = ({ fullScreen = false }) => {
+const Loader = ({ fullScreen }) => {
   return (
     <Container fullScreen={fullScreen}>
       <img src={svgLoader}/>
     </Container>
   )
+}
+
+Loader.propTypes = {
+  fullScreen: PropTypes.bool
+}
+
+Loader.defaultProps = {
+  fullScreen: false
 }
 
 export default Loader;
